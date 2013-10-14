ffd2c9df0a0c6f37b2d75177bde952eb3b9be604
Wayne <guowangyang@gmail.com>
2013-10-14 19:29:49 +0800

    + profiler.py

---
+++ profiler.py
@@ -0,0 +1,24 @@
+import time
+
+
+class Timer(object):
+    ''' a timer to record code excution time
+
+    example:
+        with Timer() as t:
+            test_func()
+        print "=> elasped lpush: %s s" % t.secs
+    '''
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.secs = self.end - self.start
+        self.msecs = self.secs * 1000  # millisecs
+        if self.verbose:
+            print 'elapsed time: %f ms' % self.msecs
